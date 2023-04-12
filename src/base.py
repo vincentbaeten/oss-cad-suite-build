d57bc1f449afde220c1e17b29e77c697db7831a4
Vincent Baeten <vincent@baeten.dev>
2023-04-12 13:43:24 +0200

    Replace yosyshq dockerhub with own ghcr.io prefix

--- src/base.py
+++ src/base.py
@@ -428,7 +428,7 @@ def executeBuild(target, arch, prefix, build_dir, output_dir, nproc, pack_source
 		else:
 			params += ['-e', '{}={}'.format(i, j)]
 	params += [
-		'yosyshq/cross-'+ arch + ':1.1',
+		'ghcr.io/vincentbaeten/oss-cad-suite-build/cross-'+ arch + ':1.1',
 		'bash', scriptfile.name
 	]
 	return run_live(params, cwd=build_dir)
@@ -726,7 +726,7 @@ def generateYaml(target, build_arch, write_to_file):
 						"      - completed\n\n".format(arch_chain[build_arch])
 	yaml_content += "jobs:\n"
 
-	BUCKET_URL = "https://github.com/yosyshq/oss-cad-suite-build/releases/download/bucket"
+	BUCKET_URL = "https://github.com/vincentbaeten/oss-cad-suite-build/releases/download/bucket"
 	for t in build_order:
 		arch = t[0]
 		target = targets[t[1]]
@@ -775,7 +775,7 @@ def generateYaml(target, build_arch, write_to_file):
 			yaml_content +="        run: echo \"date=$(date +'%Y-%m-%d')\" >> $GITHUB_OUTPUT\n"
 		yaml_content +="      - uses: actions/checkout@v3\n"
 		yaml_content +="        with:\n"
-		yaml_content +="          repository: 'yosyshq/oss-cad-suite-build'\n"
+		yaml_content +="          repository: 'vincentbaeten/oss-cad-suite-build'\n"
 		if not target.top_package:
 			yaml_content +="      - name: Cache sources\n"
 			yaml_content +="        id: cache-sources\n"
